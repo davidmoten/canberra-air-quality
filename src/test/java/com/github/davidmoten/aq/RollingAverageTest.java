5824ba2ac6224c8e60df3ed9abd1a40b536297fc
Dave Moten <davidmoten@gmail.com>
2019-12-25 11:12:27 +1100

    fix missing values

--- src/test/java/com/github/davidmoten/aq/RollingAverageTest.java
+++ src/test/java/com/github/davidmoten/aq/RollingAverageTest.java
@@ -2,12 +2,16 @@ package com.github.davidmoten.aq;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.davidmoten.kool.Stream;
 import org.ejml.data.DMatrixRMaj;
@@ -26,60 +30,104 @@ public class RollingAverageTest {
 
     @Test
     public void test() throws IOException {
-        List<Entry> list = Stream
-                .lines(() -> RollingAverage.class.getResourceAsStream("/air.csv"),
-                        StandardCharsets.UTF_8) //
-                .skip(1) //
-                .filter(x -> x.length() > 0) //
-                .map(x -> x.replaceAll("\".*\",", "")) //
-                .map(x -> x.split(",")) //
-                .filter(x -> x[12].length() > 0) //
-                .filter(x -> x[1].length() > 0) //
-                .map(x -> new Entry(x[0], toTime(x[1]), Double.parseDouble(x[12]))) //
-                .filter(x -> x.time > sdf.parse("01/11/2019 01:00:00 AM").getTime()) //
-                .filter(x -> x.name.equalsIgnoreCase("Civic")) //
-                .sorted((x, y) -> Long.compare(x.time, y.time)) //
-                .toList() //
-                .get();
-        int windowLength = 24;
-        LinearSolverDense<DMatrixRMaj> solver = LinearSolverFactory_DDRM.pseudoInverse(true);
-        DMatrixRMaj m = new DMatrixRMaj(list.size(), list.size() + windowLength - 1);
-        for (int row = 0; row < m.numRows; row++) {
-            for (int col = row; col < row + windowLength; col++) {
-                m.set(row, col, 1 / (double) windowLength);
+        for (String name : new String[] { "Civic", "Florey", "Monash" }) {
+            System.out.println(name);
+            List<Entry> list = Stream
+                    .lines(() -> RollingAverage.class.getResourceAsStream("/air.csv"),
+                            StandardCharsets.UTF_8) //
+                    .skip(1) //
+                    .filter(x -> x.length() > 0) //
+                    .map(x -> x.replaceAll("\".*\",", "")) //
+                    .map(x -> x.split(",")) //
+                    .doOnNext(x -> {
+                        if (x[12].isEmpty() && x[1].contains("12/2019")) {
+                            System.out.println(Arrays.toString(x));
+                        }
+                    }).filter(x -> x[1].length() > 0) //
+                    .map(x -> new Entry(x[0], toTime(x[1]), getDouble(x[12]))) //
+                    .filter(x -> x.time > sdf.parse("01/11/2019 01:00:00 AM").getTime()) //
+                    .filter(x -> x.name.equalsIgnoreCase("Civic")) //
+                    .sorted((x, y) -> Long.compare(x.time, y.time)) //
+                    .toList() //
+                    .map(x -> {
+                        // trim all missing entries from the start and finish
+                        List<Entry> v = Stream //
+                                .from(x) //
+                                .skipUntil(y -> y.value.isPresent()) //
+                                .reverse() //
+                                .skipUntil(y -> y.value.isPresent()) //
+                                .reverse() //
+                                .toList() //
+                                .get();
+
+                        // fix missing using average where possible or repeating previous value
+                        for (int i = 1; i < v.size() - 1; i++) {
+                            Entry entry = v.get(i);
+                            if (!entry.value.isPresent()) {
+                                if (v.get(i + 1).value.isPresent()) {
+                                    Entry entry2 = new Entry(entry.name, entry.time, Optional.of(
+                                            (v.get(i - 1).value.get() + v.get(i + 1).value.get())
+                                                    / 2));
+                                    v.set(i, entry2);
+                                } else {
+                                    Entry entry2 = new Entry(entry.name, entry.time,
+                                            Optional.of(v.get(i - 1).value.get()));
+                                    v.set(i, entry2);
+                                }
+                            }
+                        }
+
+                        // any missing entries set them to the average of the value before and after
+                        return v;
+                    }).get();
+            int windowLength = 24;
+            LinearSolverDense<DMatrixRMaj> solver = LinearSolverFactory_DDRM.pseudoInverse(true);
+            DMatrixRMaj m = new DMatrixRMaj(list.size(), list.size() + windowLength - 1);
+            for (int row = 0; row < m.numRows; row++) {
+                for (int col = row; col < row + windowLength; col++) {
+                    m.set(row, col, 1 / (double) windowLength);
+                }
             }
-        }
-        solver.setA(m);
-        DMatrixRMaj inv = new DMatrixRMaj(list.size(), list.size() + windowLength - 1);
-        solver.invert(inv);
+            solver.setA(m);
+            DMatrixRMaj inv = new DMatrixRMaj(list.size(), list.size() + windowLength - 1);
+            solver.invert(inv);
 
-        // multiply inv be entries to get original values
+            // multiply inv be entries to get original values
 
-        // Solve Ax = y for x
-        SimpleMatrix aInv = new SimpleMatrix(inv);
-        SimpleMatrix y = new SimpleMatrix(list.size(), 1);
-        for (int i = 0; i < list.size(); i++) {
-            y.set(i, 0, list.get(i).value);
-        }
-        // Then Ainv . A x = Ainv . y
-        // Therefore x = Ainv . y
+            // Solve Ax = y for x
+            SimpleMatrix aInv = new SimpleMatrix(inv);
+            SimpleMatrix y = new SimpleMatrix(list.size(), 1);
+            for (int i = 0; i < list.size(); i++) {
+                y.set(i, 0, list.get(i).value.get());
+            }
+            // Then Ainv . A x = Ainv . y
+            // Therefore x = Ainv . y
 
-        System.out.println("aInv: " + aInv.numRows() + "x" + aInv.numCols());
-        System.out.println("y: " + y.numRows() + "x" + y.numCols());
-        SimpleMatrix z = aInv.mult(y);
-        System.out.println("-------------");
-        System.out.println("entries length=" + list.size());
-        System.out.println("z size=" + z.numRows());
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(
-                    sdf.format(new Date(list.get(i).time)) + "\t" + z.get(i + windowLength - 1, 0));
+            SimpleMatrix z = aInv.mult(y);
+
+            File outfile = new File("src/output/" + name + ".csv");
+            outfile.getParentFile().mkdirs();
+            try (PrintStream out = new PrintStream(outfile)) {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                for (int i = 0; i < list.size(); i++) {
+                    out.println(sdf.format(new Date(list.get(i).time)) + "\t"
+                            + z.get(i + windowLength - 1, 0));
+                }
+            }
         }
     }
 
     @Test
     public void testSdf() throws ParseException {
         assertEquals(1576461600000L, sdf.parse("16/12/2019 01:00:00 PM").getTime());
+    }
+
+    private static Optional<Double> getDouble(String s) {
+        try {
+            return Optional.of(Double.parseDouble(s));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
     }
 
     private static long toTime(String s) {
@@ -93,9 +141,9 @@ public class RollingAverageTest {
     static final class Entry {
         final String name;
         final long time;
-        final double value;
+        final Optional<Double> value;
 
-        Entry(String name, long time, double value) {
+        Entry(String name, long time, Optional<Double> value) {
             this.name = name;
             this.time = time;
             this.value = value;
