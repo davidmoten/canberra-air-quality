f774fcda13dee85277a11f110b9347a37c552543
Dave Moten <davidmoten@gmail.com>
2019-12-26 16:32:49 +1030

    dev

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 set -e 
 set -x
 wget -O src/test/resources/air.csv https://www.data.act.gov.au/api/views/94a5-zqnn/rows.csv?accessType=DOWNLOAD
+git commit -am "update air.csv"
+git push
 mvn clean install
 cd ../davidmoten.github.io
 git pull
@@ -9,5 +11,6 @@ mkdir -p canberra-air-quality
 cd -
 cp target/*.png ../davidmoten.github.io/canberra-air-quality
 cd ../davidmoten.github.io
-gitc "upgrade air quality pngs"
+git commit -am "upgrade air quality pngs"
+git push
 
